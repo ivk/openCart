4bcb9442d2f72e602f9e64d35c5b8bd76bcaede3
ivk <i_smith@mail.ru>
2024-08-21 13:08:12 +0300

    headless mode added

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+from random import choices
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -5,24 +7,34 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
+TIMEOUT = 1
+
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="chrome", help="Browser to run tests (chrome/firefox)")
-    parser.addoption("--base_url", action="store", default="https://www.python.org", help="Base URL for the tests")
+    parser.addoption("--browser", action="store", default="chrome", help="Browser to run tests (chrome/firefox)", choices=('chrome', 'firefox'))
+    parser.addoption("--headless", action="store", default="false", help="Use headless browser (false/true)", choices=('true', 'false'))
+    parser.addoption("--base_url", action="store", default="http://192.168.10.79:8081/", help="Base URL for the tests")
 
 
 @pytest.fixture(scope="session")
 def browser(request):
     browser_name = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
 
     if browser_name == "chrome":
         options = ChromeOptions()
+        if headless == 'true':
+            options.add_argument("-headless")
+            options.add_argument('window-size=1920,1080')
         service = ChromeService()
         browser = webdriver.Chrome(service=service, options=options)
     elif browser_name == "firefox":
         options = FirefoxOptions()
-        # options.binary_location = '/home/irina/bin/'
-        # service = FirefoxService(executable_path='/home/irina/bin/geckodriver')
+        if headless == 'true':
+            # for the god sake, don't use it ever! tests become smelly
+            options.add_argument("-headless")
+            options.add_argument("--width=1920")
+            options.add_argument("--height=1080")
         service = FirefoxService()
         browser = webdriver.Firefox(options=options, service=service)
 
---
+++ tests/test_pages.py
@@ -0,0 +1,87 @@
+"""
+Часть 2
+2.1 Написать тесты проверяющие элементарное наличие элементов на разных страницах приложения opencart
+2.2 Реализовать минимум пять тестов (одни тест = одна страница приложения)
+2.3 Использовать методы явного ожидания элементов
+
+Покрыть нужно:
+
+Главную
+Каталог
+Карточку товара
+Страницу логина в админку /administration
+Страницу регистрации пользователя (/index.php?route=account/register)
+Какие именно элементы проверять определить самостоятельно, но не меньше 5 для каждой страницы
+"""
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+TIMEOUT = 1
+
+
+def test_main(browser, base_url):
+    browser.get(base_url)
+
+    WebDriverWait(browser, timeout=TIMEOUT)
+
+    assert "Your Store" in browser.title
+    assert browser.find_element(By.ID, "logo")
+
+    product_cards = browser.find_elements(By.CLASS_NAME, "product-thumb")
+    assert len(product_cards) == 4
+
+    assert browser.find_element(By.ID, "carousel-banner-1")
+    assert "OpenCart" in browser.find_element(By.TAG_NAME, "footer").text
+
+
+def test_catalogue(browser, base_url):
+    browser.get(f"{base_url}/catalog/desktops")
+    assert WebDriverWait(browser, TIMEOUT).until(lambda x: x.find_element(By.TAG_NAME, "main"))
+    assert browser.find_element(By.ID, 'product-category')
+    assert browser.find_element(By.CSS_SELECTOR, 'ul.breadcrumb')
+    assert browser.find_element(By.CSS_SELECTOR, '#content h2')
+    assert browser.find_element(By.ID, 'product-list')
+
+    products = browser.find_elements(By.CLASS_NAME, 'product-thumb')
+    assert len(products) > 4
+
+    assert browser.find_element(By.CSS_SELECTOR, 'ul.pagination')
+
+
+def test_card(browser, base_url):
+    browser.get(f"{base_url}/product/desktops/macbook")
+    assert WebDriverWait(browser, TIMEOUT).until(lambda x: x.find_element(By.TAG_NAME, "main"))
+    assert browser.find_element(By.CSS_SELECTOR, '#product-info #content')
+    assert browser.find_element(By.TAG_NAME, 'h1')
+    price = browser.find_element(By.CLASS_NAME, 'price-new').text
+    assert '$' in price
+    assert float(price[1:]) > 100
+
+
+
+def test_admin_login_page(browser, base_url):
+    browser.get(f"{base_url}/administration")
+
+    element = WebDriverWait(browser, TIMEOUT).until(lambda x: x.find_element(By.ID, "content"))
+    assert "Please enter your login details." in element.text
+
+    assert "Administration" in browser.title
+    assert browser.find_element(By.NAME, "username")
+    assert browser.find_element(By.NAME, "password")
+
+    btn = browser.find_element(By.TAG_NAME, "button")
+    assert btn.text == "Login"
+    assert btn.is_enabled()
+    assert 'btn' in btn.get_attribute('class')
+
+
+def test_user_registration(browser, base_url):
+    browser.get(f"{base_url}/?route=account/register")
+    assert WebDriverWait(browser, TIMEOUT).until(lambda x: x.find_element(By.TAG_NAME, "main"))
+    assert browser.find_element(By.TAG_NAME, 'nav')
+    assert browser.find_element(By.TAG_NAME, 'header')
+    assert browser.find_element(By.TAG_NAME, 'footer')
+    assert browser.find_element(By.CSS_SELECTOR, "main form")
+    assert browser.find_element(By.CSS_SELECTOR, ".text-end button").is_enabled()
+
+
